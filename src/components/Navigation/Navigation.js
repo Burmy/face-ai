60ef340bdefcb92ad15edae8fb761524a0342469
Burmy <62005549+Burmy@users.noreply.github.com>
2020-08-31 20:00:19 -0700

    fron end fixing

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -7,17 +7,17 @@ const Navigation = ({ onRouteChange, isSignedIn }) => {
         return (
             < nav style={{ display: 'flex', justifyContent: 'flex-end' }}>
                 <p onClick={() => onRouteChange('signout')}
-                    className='f5 ph4 reg pointer br3'> Sign Out </p> {/* className card/reg in Signin.css */}
+                    className='btn'> Sign Out </p>
             </ nav >
         );
     } else {
         return (
             < nav style={{ display: 'flex', justifyContent: 'flex-end' }}>
                 <p onClick={() => onRouteChange('signin')}
-                    className='f5 ph4 reg pointer br3 '>Sign In</p> {/* className card/reg in Signin.css */}
+                    className='btn'>Sign In</p>
 
                 <p onClick={() => onRouteChange('register')}
-                    className='f5 ph4 reg pointer br3'>Register</p> {/* className card/reg in Signin.css */}
+                    className='btn'>Register</p>
 
             </ nav >
         );
