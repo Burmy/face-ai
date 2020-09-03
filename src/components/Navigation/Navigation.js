e6341e2e550d4eb63831963033bed424e72f7e6b
Burmy <62005549+Burmy@users.noreply.github.com>
2020-09-02 19:40:42 -0700

    nav bar fix

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -7,17 +7,17 @@ const Navigation = ({ onRouteChange, isSignedIn }) => {
         return (
             < nav style={{ display: 'flex', justifyContent: 'flex-end' }}>
                 <p onClick={() => onRouteChange('signout')}
-                    className='f3 link dim black underline pa3 pointer'> Sign Out </p>
+                    className='f3 btn link dim pa3 pointer'> Sign Out </p>
             </ nav >
         );
     } else {
         return (
             < nav style={{ display: 'flex', justifyContent: 'flex-end' }}>
                 <p onClick={() => onRouteChange('signin')}
-                    className='f3 link dim black underline pa3 pointer'>Sign In</p>
+                    className='f3 btn link dim pa3 pointer'>Sign In</p>
 
                 <p onClick={() => onRouteChange('register')}
-                    className='f3 link dim black underline pa3 pointer'>Register</p>
+                    className='f3 btn link dim pa3 pointer'>Register</p>
 
             </ nav >
         );
