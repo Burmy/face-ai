771e37a02614f8b3f369b1888380d93fa226c7bd
Burmy <62005549+Burmy@users.noreply.github.com>
2020-09-04 15:58:32 -0700

    fixing ui

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -10,7 +10,7 @@ const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
             <div className='center'>
                 <div className='form center pa4 shadow-5'>
                     <input className='input f4 ps2 w-70 center' type='tex' placeholder="Enter Image URL..." onChange={onInputChange} />
-                    <button className='btn w-30 f4 link pv2 ' onClick={onButtonSubmit} > <span> Detect </span></button>
+                    <button className='detect w-30 f4 link pv2 ' onClick={onButtonSubmit} > <span> Detect </span></button>
                 </div>
             </div>
         </div>
